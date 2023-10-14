67bb6c8550b53161b3c5472d5f3d977bd2bf86d2
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2023-10-14 12:14:11 -0300

    chore: add prettier and eslint with react-native-community config

--- src/components/restaurant-info.js
+++ src/components/restaurant-info.js
@@ -1,6 +1,6 @@
-import {Text, View} from "react-native";
+import React from "react";
+import { Text } from "react-native";
 
 export const RestaurantInfo = () => {
-    return <Text>Restaurant Info</Text>
-
-}
+  return <Text>Restaurant Info</Text>;
+};
--- src/screens/restaurant.screen.js
+++ src/screens/restaurant.screen.js
@@ -1,32 +1,33 @@
-import {StatusBar} from "expo-status-bar";
-import {Platform, SafeAreaView, StyleSheet, Text, View} from "react-native";
-import {Searchbar} from "react-native-paper";
-import {RestaurantInfo} from "../components/restaurant-info";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Platform, SafeAreaView, StyleSheet, View } from "react-native";
+import { Searchbar } from "react-native-paper";
+import { RestaurantInfo } from "../components/restaurant-info";
 
 export const RestaurantScreen = () => (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-            <Searchbar/>
-        </View>
-        <View style={styles.list}>
-            <RestaurantInfo />
-        </View>
-        <StatusBar style="auto"/>
-    </SafeAreaView>
+  <SafeAreaView style={styles.container}>
+    <View style={styles.search}>
+      <Searchbar />
+    </View>
+    <View style={styles.list}>
+      <RestaurantInfo />
+    </View>
+    <StatusBar style="auto" />
+  </SafeAreaView>
 );
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    search: {
-        flex: 0.1,
-        justifyContent: 'center',
-        paddingTop: Platform.OS === 'android' ? 30 : 0,
-        padding: 10
-    },
-    list: {
-        flex: 1,
-        padding: 10
-    }
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  search: {
+    flex: 0.1,
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? 30 : 0,
+    padding: 10,
+  },
+  list: {
+    flex: 1,
+    padding: 10,
+  },
 });
