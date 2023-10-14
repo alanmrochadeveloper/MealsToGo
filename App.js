13004e06cbaa08e716103d54c38f9af541dd1b6f
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2023-10-14 16:08:05 -0300

    refactor: few tweaks

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import {RestaurantScreen} from "./src/screens/restaurant.screen";
-import {StatusBar as ExpoStatusBar }from 'expo-status-bar'
+import React from "react";
+import { RestaurantScreen } from "./src/screens/restaurant.screen";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 
 export default function App() {
-    return <>
-        <RestaurantScreen/>
-        <ExpoStatusBar />
-    </>;
+  return (
+    <>
+      <RestaurantScreen />
+      <ExpoStatusBar />
+    </>
+  );
 }
-
