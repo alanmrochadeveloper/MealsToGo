87d03fff5e29a8c8043768bea6b85b98567fd602
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2023-10-14 10:24:37 -0300

    refator: improve the main structure a bit

--- App.js
+++ App.js
@@ -1,34 +1,10 @@
-import {StatusBar} from 'expo-status-bar';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {RestaurantScreen} from "./src/screens/restaurant.screen";
+import {StatusBar as ExpoStatusBar }from 'expo-status-bar'
 
 export default function App() {
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.search}>
-                <Text>search</Text>
-            </View>
-            <View style={styles.list}>
-                <Text>list</Text>
-            </View>
-            <StatusBar style="auto"/>
-        </SafeAreaView>
-    );
+    return <>
+        <RestaurantScreen/>
+        <ExpoStatusBar />
+    </>;
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    search: {
-        flex: 0.1,
-        backgroundColor: 'green',
-        justifyContent: 'center',
-        paddingLeft: 10
-    },
-    list: {
-        flex: 1,
-        backgroundColor: 'blue',
-        padding: 10
-    }
-});
---
+++ src/components/restaurant-info.js
@@ -0,0 +1,6 @@
+import {Text, View} from "react-native";
+
+export const RestaurantInfo = () => {
+    return <Text>Restaurant Info</Text>
+
+}
