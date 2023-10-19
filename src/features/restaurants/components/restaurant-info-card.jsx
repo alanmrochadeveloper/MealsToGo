dac7d4095094e6f6830f5ba28ead7d3905e28eb0
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2023-10-18 23:15:51 -0300

    refactor: finished up the the card component

--- src/features/restaurants/components/restaurant-info-card.jsx
+++ src/features/restaurants/components/restaurant-info-card.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, Image } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components";
 import {
@@ -12,6 +12,7 @@ import {
 } from "@expo-google-fonts/lato";
 import { SvgXml } from "react-native-svg";
 import star from "../../../assets/star";
+import open from "../../../assets/open";
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const [OswaldLoaded] = useOswaldFonts({
@@ -52,6 +53,35 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     flex-direction: row;
   `;
 
+  const Open = styled.View`
+    margin-top: ${({ theme }) => theme.space[2]};
+    margin-bottom: ${({ theme }) => theme.space[2]};
+    flex-direction: row;
+  `;
+
+  const OpenRatingContainer = styled.View`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+  `;
+
+  const ClosedTemporarily = styled.View`
+    flex-direction: row;
+    align-items: center;
+    margin-right: 15px;
+  `;
+
+  const ClosedTempText = styled.Text`
+    color: ${({ theme }) => theme.colors.text.error};
+    text-transform: uppercase;
+    font-weight: ${({ theme }) => theme.fontWeights.bold};
+    margin-right: 6px;
+  `;
+
+  const AvailableContainer = styled.View`
+    flex-direction: row;
+  `;
+
   const {
     name = "test 3",
     icon = "https://images.freeimages.com/images/large-previews/510/trash-icon-1525880.jpg",
@@ -61,23 +91,40 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     address = "Avenue ST de Elises",
     isOpenNow = true,
     rating = 5,
-    isClosedTemporarily = false,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const ratings = Array.from(new Array(Math.floor(rating)));
 
-  const rate = () =>
-    ratings.map(() => <SvgXml xml={star} width={20} height={20} />);
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover source={{ uri: photos[0] }} />
       <Info>
         <RestaurantCardTitle>{name}</RestaurantCardTitle>
-        <Rating>
-          {ratings.map(() => (
-            <SvgXml xml={star} width={20} height={20} />
-          ))}
-        </Rating>
+        <OpenRatingContainer>
+          <Rating>
+            {ratings.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <AvailableContainer>
+            {isClosedTemporarily && (
+              <ClosedTemporarily>
+                <ClosedTempText>temporarily closed</ClosedTempText>
+                <Image
+                  source={{ uri: icon }}
+                  alt="icone qualquer"
+                  style={{ width: 20, height: 20 }}
+                />
+              </ClosedTemporarily>
+            )}
+            {isOpenNow && (
+              <Open>
+                <SvgXml xml={open} width={20} height={20} />
+              </Open>
+            )}
+          </AvailableContainer>
+        </OpenRatingContainer>
         <RestaurantCardAddress>{address}</RestaurantCardAddress>
       </Info>
     </RestaurantCard>
